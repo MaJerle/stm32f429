9077974dfdee2a6380f3578bd14a6de9ff241677
tilz0R <tilen.majerle@gmail.com>
2014-04-21 22:37:04 +0200

    Updated main project file
    Added comments

--- 02-STM32F429_LED_BUTTON/main.c
+++ 02-STM32F429_LED_BUTTON/main.c
@@ -1,5 +1,12 @@
 /**
- * Includes
+ *	Leds and button example
+ *
+ *	@author 	Tilen Majerle
+ *	@email		tilen@majerle.eu
+ *	@website	http://majerle.eu
+ *	@version 	v1.0
+ *	@gcc		v4.7 20013qr3
+ *	@ide		CooCox CoIDE v1.7.6
  */
 #include "stm32f4xx.h"
 #include "tm_stm32f4_disco.h"
