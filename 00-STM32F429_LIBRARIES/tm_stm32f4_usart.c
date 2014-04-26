3e4f71296f3a7292f8d9ce73b66a8a40767828b2
tilz0R <tilen.majerle@gmail.com>
2014-04-26 15:28:40 +0200

    Now works for all 8 U(S)ARTs
    Library can now be used with all 8 USARTs, which are supported in
    STM32F429

--- 00-STM32F429_LIBRARIES/tm_stm32f4_usart.c
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_usart.c
@@ -5,58 +5,56 @@
  *
  */
 int8_t TM_USART_Buffer[8][TM_USART_BUFFER_SIZE];
-uint16_t tm_usart_buf_in[8] = {0, 0, 0, 0, 0, 0};
-uint16_t tm_usart_buf_out[8] = {0, 0, 0, 0, 0, 0};
-uint16_t tm_usart_buf_num[8] = {0, 0, 0, 0, 0, 0};
+uint16_t tm_usart_buf_in[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+uint16_t tm_usart_buf_out[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+uint16_t tm_usart_buf_num[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-void TM_USART_Init(USART_TypeDef* USARTx, uint32_t baudrate) {
-	/**
-	 * Only USART1 for now
-	 */
-	if (USARTx != USART1) {
-		return;
-	}
+void TM_USART_Init(USART_TypeDef* USARTx, TM_USART_PinsPack_t pinspack, uint32_t baudrate) {
 	/**
 	 * Initialization structures declared
 	 *
-	 * GPIO: General Purpose Input Output
 	 * USART: Universal Synchronous/Asynchronous Receiver/Transmitter
 	 * NVIC: Nested Vector Interrupt Controller
 	 */
-	GPIO_InitTypeDef 	GPIO_InitStruct;
 	USART_InitTypeDef 	USART_InitStruct;
 	NVIC_InitTypeDef	NVIC_InitStruct;
 
-	// Enable clock for GPIOA
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-	/**
-	 * Tell pins PA9 and PA10 which alternating function you will use
-	 * @important Make sure, these lines are before pins configuration!
+	/*
+	 * Initialize USARTx pins
+	 * Set channel for USARTx NVIC
 	 */
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
-
-	/**
-	 * Configure pins PA9 (TX) and PA10 (RX) for USART1
-	 * Set mode to AF (alternating function)
-	 * Set output type to push-pull
-	 * Enable pull UP resistor
-	 * Set speed to 100MHz
-	 *
-	 * Initialize GPIOA
-	 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	if (USARTx == USART1) {
+		TM_USART1_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	} else if (USARTx == USART2) {
+		TM_USART2_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = USART2_IRQn;
+	} else if (USARTx == USART3) {
+		TM_USART3_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = USART3_IRQn;
+	} else if (USARTx == UART4) {
+		TM_UART4_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = UART4_IRQn;
+	} else if (USARTx == UART5) {
+		TM_UART5_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = UART5_IRQn;
+	} else if (USARTx == USART6) {
+		TM_USART6_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = USART6_IRQn;
+	} else if (USARTx == UART7) {
+		TM_UART7_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = UART7_IRQn;
+	} else if (USARTx == UART8) {
+		TM_UART8_InitPins(pinspack);
+		NVIC_InitStruct.NVIC_IRQChannel = UART8_IRQn;
+	}
 
 	/**
 	 * Enable clock for USART1 peripheral
 	 */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	if (USARTx == USART1 || USARTx == USART6) {
+
+	}
 
 	/**
 	 * Set Baudrate to value you pass to function
@@ -66,8 +64,8 @@ void TM_USART_Init(USART_TypeDef* USARTx, uint32_t baudrate) {
 	 * Set 1 stop bit
 	 * Set Data bits to 8
 	 *
-	 * Initialize USART1
-	 * Activate USART1
+	 * Initialize USARTx
+	 * Activate USARTx
 	 */
 	USART_InitStruct.USART_BaudRate = baudrate;
 	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
@@ -75,26 +73,313 @@ void TM_USART_Init(USART_TypeDef* USARTx, uint32_t baudrate) {
 	USART_InitStruct.USART_Parity = USART_Parity_No;
 	USART_InitStruct.USART_StopBits = USART_StopBits_1;
 	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_Init(USART1, &USART_InitStruct);
-	USART_Cmd(USART1, ENABLE);
+	USART_Init(USARTx, &USART_InitStruct);
+	USART_Cmd(USARTx, ENABLE);
 
 	/**
 	 * Enable RX interrupt
 	 */
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	USART_ITConfig(USARTx, USART_IT_RXNE, ENABLE);
 
 	/**
-	 * Set Channel to USART1
-	 * Set Channel Cmd to enable. That will enable USART1 channel in NVIC
-	 * Set Both priorities to 0. This means high priority
+	 * Set Channel Cmd to enable. That will enable USARTx channel in NVIC
+	 * Set NVIC_IRQChannelPreemptionPriority. This means high priority for USARTs
+	 * USARTx with lower x has high subpriority
 	 *
 	 * Initialize NVIC
 	 */
-	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = TM_USART_GetUsartNumber(USARTx);
 	NVIC_Init(&NVIC_InitStruct);
+}
+
+void TM_USART1_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for USART1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+
+		//Alternating function
+		GPIO_Init(GPIOA, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+
+		//Alternating function
+		GPIO_Init(GPIOB, &GPIO_InitStruct);
+	}
+}
+
+void TM_USART2_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for USART2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+
+		//Alternating function
+		GPIO_Init(GPIOA, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+		GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
+		GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6;
+
+		//Alternating function
+		GPIO_Init(GPIOD, &GPIO_InitStruct);
+	}
+}
+
+void TM_USART3_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for USART3
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
+		GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+
+		//Alternating function
+		GPIO_Init(GPIOB, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
+		//                         TX            RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+
+		//Alternating function
+		GPIO_Init(GPIOC, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_3) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+		GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
+		GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+
+		//Alternating function
+		GPIO_Init(GPIOD, &GPIO_InitStruct);
+	}
+}
+
+void TM_UART4_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for UART4
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_UART4);
+		GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_UART4);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+
+		//Alternating function
+		GPIO_Init(GPIOA, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_UART4);
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_UART4);
+		//                         TX            RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+
+		//Alternating function
+		GPIO_Init(GPIOC, &GPIO_InitStruct);
+	}
+}
+
+void TM_UART5_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for UART5
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+		//Alternating function
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_UART5);
+		GPIO_PinAFConfig(GPIOD, GPIO_PinSource2, GPIO_AF_UART5);
+		//                         TX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
+		GPIO_Init(GPIOC, &GPIO_InitStruct);
+		//                         RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+		GPIO_Init(GPIOD, &GPIO_InitStruct);
+	}
+}
+
+void TM_USART6_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for USART6
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
+		GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_USART6);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+
+		//Alternating function
+		GPIO_Init(GPIOC, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+
+		GPIO_PinAFConfig(GPIOG, GPIO_PinSource14, GPIO_AF_USART6);
+		GPIO_PinAFConfig(GPIOG, GPIO_PinSource9, GPIO_AF_USART6);
+		//                         TX            RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_9;
+
+		//Alternating function
+		GPIO_Init(GPIOG, &GPIO_InitStruct);
+	}
+}
+
+void TM_UART7_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for UART7
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART7, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+		GPIO_PinAFConfig(GPIOE, GPIO_PinSource8, GPIO_AF_UART7);
+		GPIO_PinAFConfig(GPIOE, GPIO_PinSource7, GPIO_AF_UART7);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_7;
+
+		//Alternating function
+		GPIO_Init(GPIOE, &GPIO_InitStruct);
+	} else if (pinspack == TM_USART_PinsPack_2) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+
+		GPIO_PinAFConfig(GPIOF, GPIO_PinSource7, GPIO_AF_UART7);
+		GPIO_PinAFConfig(GPIOF, GPIO_PinSource6, GPIO_AF_UART7);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6;
+
+		//Alternating function
+		GPIO_Init(GPIOF, &GPIO_InitStruct);
+	}
+}
+
+void TM_UART8_InitPins(TM_USART_PinsPack_t pinspack) {
+	//Enable clock for UART8
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART8, ENABLE);
+
+	//GPIO Structure
+	GPIO_InitTypeDef 	GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+
+	if (pinspack == TM_USART_PinsPack_1) {
+		// Enable clock for GPIOA
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+		GPIO_PinAFConfig(GPIOE, GPIO_PinSource1, GPIO_AF_UART8);
+		GPIO_PinAFConfig(GPIOE, GPIO_PinSource0, GPIO_AF_UART8);
+		//                         TX           RX
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_0;
+
+		//Alternating function
+		GPIO_Init(GPIOE, &GPIO_InitStruct);
+	}
 }
 
 void TM_USART_Putc(USART_TypeDef* USARTx, volatile char c) {
@@ -114,12 +399,68 @@ void USART1_IRQHandler(void) {
 	//Check if interrupt was because data is received
 	if (USART_GetITStatus(USART1, USART_IT_RXNE)) {
 		//Put received data into internal buffer
-		TM_USART_InsertToBuffer(USART1, USART1->DR);
+		TM_USART_InsertToBuffer(0, USART1->DR);
 	}
 }
 
-void TM_USART_InsertToBuffer(USART_TypeDef* USARTx, char c) {
-	uint8_t usart_num = TM_USART_GetUsartNumber(USARTx);
+void USART2_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(USART2, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(1, USART2->DR);
+	}
+}
+
+void USART3_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(USART3, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(2, USART3->DR);
+	}
+}
+
+void UART4_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(UART4, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(3, UART4->DR);
+	}
+}
+
+void UART5_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(UART5, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(4, UART5->DR);
+	}
+}
+
+void USART6_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(USART6, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(5, USART6->DR);
+	}
+}
+
+void UART7_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(UART7, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(6, UART7->DR);
+	}
+}
+
+void UART8_IRQHandler(void) {
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(UART8, USART_IT_RXNE)) {
+		//Put received data into internal buffer
+		TM_USART_InsertToBuffer(7, UART8->DR);
+	}
+}
+
+void TM_USART_InsertToBuffer(uint8_t usart_num, char c) {
+	//uint8_t usart_num = TM_USART_GetUsartNumber(USARTx);
 	//Still available space in buffer
 	if (tm_usart_buf_num[usart_num] < TM_USART_BUFFER_SIZE) {
 		if (tm_usart_buf_in[usart_num] > (TM_USART_BUFFER_SIZE - 1)) {
--- 00-STM32F429_LIBRARIES/tm_stm32f4_usart.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_usart.h
@@ -20,6 +20,7 @@
 
 #ifndef TM_USART_
 #define TM_USART_
+
 /**
  * Includes
  */
@@ -28,6 +29,7 @@
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_usart.h"
+
 /**
  * Buffer size
  */
@@ -36,23 +38,152 @@
 #endif
 
 /**
- * Initialize USART
+ * When you initialize USARTx, you have to select which pins pack you will use
  *
- * Currently supported only USART1
+ * Pinspack 3 can be used only for USART3
+ */
+typedef enum {
+	TM_USART_PinsPack_1,
+	TM_USART_PinsPack_2,
+	TM_USART_PinsPack_3
+} TM_USART_PinsPack_t;
+
+typedef struct {
+	GPIO_TypeDef* GPIOx;
+	uint16_t RCCx;
+	uint16_t GPIO_Pin;
+	uint8_t GPIO_PinSource;
+} TM_USART_Pin_t;
+
+/**
+ * Initialize USARTx
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
- *	- uint32_t baudrate: baudrate for USART
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 		- TM_USART_PinsPack_2
+ * 		- TM_USART_PinsPack_3 (only for USART3)
+ *	- uint32_t baudrate: baudrate for USARTx
  */
-extern void TM_USART_Init(USART_TypeDef* USARTx, uint32_t baudrate);
+extern void TM_USART_Init(USART_TypeDef* USARTx, TM_USART_PinsPack_t pinspack, uint32_t baudrate);
+
+/**
+ * Initialize USART1 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PA9
+ * 			- RX: PA10
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PB6
+ * 			- RX: PB7
+ */
+extern void TM_USART1_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize USART2 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PA2
+ * 			- RX: PA3
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PD5
+ * 			- RX: PD6
+ */
+extern void TM_USART2_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize USART3 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PB10
+ * 			- RX: PB11
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PC10
+ * 			- RX: PC11
+ * 		- TM_USART_PinsPack_3
+ * 			- TX: PD8
+ * 			- RX: PD9
+ */
+extern void TM_USART3_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize UART4 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PA0
+ * 			- RX: PA1
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PC10
+ * 			- RX: PC11
+ */
+extern void TM_UART4_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize UART5 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PC12
+ * 			- RX: PD2
+ */
+extern void TM_UART5_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize USART6 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PC6
+ * 			- RX: PC7
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PG14
+ * 			- RX: PG9
+ */
+extern void TM_USART6_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize UART7 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PE8
+ * 			- RX: PE7
+ * 		- TM_USART_PinsPack_2
+ * 			- TX: PF7
+ * 			- RX: PF6
+ */
+extern void TM_UART7_InitPins(TM_USART_PinsPack_t pinspack);
+
+/**
+ * Initialize UART8 pins
+ *
+ * Parameters:
+ * 	- TM_USART_PinsPack_t pinspack: Pinspack used
+ * 		- TM_USART_PinsPack_1
+ * 			- TX: PE1
+ * 			- RX: PE0
+ */
+extern void TM_UART8_InitPins(TM_USART_PinsPack_t pinspack);
 
 /**
  * Put character to USARTx
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
  *	- volatile char c: char to be sent to USART
  */
 extern void TM_USART_Putc(USART_TypeDef* USARTx, volatile char c);
@@ -62,7 +193,7 @@ extern void TM_USART_Putc(USART_TypeDef* USARTx, volatile char c);
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
  *	- char* str: pointer to first character in string
  */
 extern void TM_USART_Puts(USART_TypeDef* USARTx, char* str);
@@ -72,7 +203,7 @@ extern void TM_USART_Puts(USART_TypeDef* USARTx, char* str);
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
  *
  * Returned character if exists in buffer, otherwise zero
  */
@@ -90,18 +221,18 @@ extern void USART1_IRQHandler(void);
  * Called in interrupt
  *
  * Parameters:
- * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 	- uint8_t USARTnum
+ * 		- USART number: starting from 0
  * 	- char c: character to be inserted in buffer
  */
-extern void TM_USART_InsertToBuffer(USART_TypeDef* USARTx, char c);
+extern void TM_USART_InsertToBuffer(uint8_t USARTnum, char c);
 
 /**
  * Check's if internal buffer is empty
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
  *
  * Returns 1 if buffer is empty, otherwise 0
  */
@@ -114,7 +245,7 @@ extern uint8_t TM_USART_BufferEmpty(USART_TypeDef* USARTx);
  *
  * Parameters:
  * 	- USART_TypeDef* USARTx: which USART channel
- * 		USART1, USART2, USART3, UART4, UART5, USART6
+ * 		USART1, USART2, USART3, UART4, UART5, USART6, UART7. UART8
  *
  * Returns x - 1 number.
  */
