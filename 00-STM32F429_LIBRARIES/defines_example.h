180db25d59f570960a289d568affbf1ac144ba68
tilz0R <tilen.majerle@gmail.com>
2014-04-26 16:42:21 +0200

    Defines.h filename changed

---
+++ 00-STM32F429_LIBRARIES/defines_example.h
@@ -0,0 +1,16 @@
+/**
+ *  Defines for your entire project at one place
+ * 
+ *	@author 	Tilen Majerle
+ *	@email		tilen@majerle.eu
+ *	@website	http://majerle.eu
+ *	@version 	v1.0
+ *	@gcc		v4.7 20013qr3
+ *	@ide		CooCox CoIDE v1.7.6
+ */
+#ifndef TM_DEFINES_H
+#define TM_DEFINES_H
+
+//Put your global defines for all libraries here used in your project
+
+#endif
