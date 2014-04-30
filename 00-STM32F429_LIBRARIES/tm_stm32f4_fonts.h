45214585e0032b2917c8133321d98907dcce10cc
tilz0R <tilen.majerle@gmail.com>
2014-04-30 14:29:42 +0200

    Fonts for ILI9341 added

---
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_fonts.h
@@ -0,0 +1,16 @@
+#ifndef TM_FONTS_H
+#define TM_FONTS_H
+
+#include "stm32f4xx.h"
+#include "defines.h"
+/**
+ * Font struct
+ */
+typedef struct {
+	uint8_t FontWidth;
+	uint8_t FontHeight;
+	const uint16_t *data;
+} TM_FontDef_t;
+
+#endif
+
