906386f9239ce85fca86391d1bf5bc1bfb0a9e9b
tilz0R <tilen.majerle@gmail.com>
2014-05-03 00:32:46 +0200

    Button library updated

--- 00-STM32F429_LIBRARIES/tm_stm32f4_ili9341_button.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_ili9341_button.h
@@ -30,18 +30,34 @@
 #include "tm_stm32f4_fonts.h"
 #include "tm_stm32f4_stmpe811.h"
 
+// Maximum number of buttons available
 #ifndef TM_ILI9341_BUTTON_MAX_BUTTONS
-#define TM_ILI9341_BUTTON_MAX_BUTTONS	20	// Maximum number of buttons available
+#define TM_ILI9341_BUTTON_MAX_BUTTONS	20
 #endif
 
 //Button flags
-#define TM_BUTTON_FLAG_ENABLED			0x0001	
-#define TM_BUTTON_FLAG_NOBORDER			0x0002
-#define TM_BUTTON_FLAG_IMAGE			0x0004
-#define TM_BUTTON_FLAG_NOLABEL			0x0008
-#define TM_BUTTON_FLAG_USED				0x8000
+#define TM_BUTTON_FLAG_ENABLED			0x0001	// Button is enabled - internal flag
+#define TM_BUTTON_FLAG_NOBORDER			0x0002	// No border used in
+#define TM_BUTTON_FLAG_IMAGE			0x0004	// Use image for background
+#define TM_BUTTON_FLAG_NOLABEL			0x0008	// Do not display button label
+#define TM_BUTTON_FLAG_USED				0x8000	// Button is used - internal flag
 
-
+/**
+ * Button options struct
+ *
+ * Parameters:
+ *	- uint16_t x: X coordinate of top left button corner
+ *	- uint16_t y: Y coordinate of top left button corner
+ *	- uint16_t width: button width in pixels
+ *	- uint16_t height: button height in pixels
+ *	- uint16_t background: 16bit background color
+ *	- uint16_t borderColor: 16bit border color
+ *	- uint16_t flags: Button flags
+ *	- char *label: pointer to first character of label
+ *	- TM_FontDef_t *font: pointer to font structure
+ *	- uint16_t color: label color
+ *	- uint16_t *image: pointer to location at first color for pixel
+ */
 typedef struct {
 	uint16_t x;
 	uint16_t y;
@@ -58,20 +74,72 @@ typedef struct {
 
 extern TM_ILI9341_Button_t TM_ILI9341_Buttons[TM_ILI9341_BUTTON_MAX_BUTTONS];
 
+/**
+ * Add new button to library
+ *
+ * Parameters:
+ * 	- TM_ILI9341_Button_t *button: pointer to TM_ILI9341_Button_t structure
+ *
+ * Returns button id on success (starting from 0), or -1 on failure
+ */
 extern int8_t TM_ILI9341_Button_Add(TM_ILI9341_Button_t *button);
 
-extern void TM_ILI9341_Button_DrawAll(void);
-
+/**
+ * Draw button on LCD
+ *
+ * Parameters:
+ * 	uint8_t id: button id to draw
+ *
+ * Return SUCCESS on success, otherwise ERROR
+ */
 extern ErrorStatus TM_ILI9341_Button_Draw(uint8_t id);
 
+/**
+ * Draw all buttons in library
+ *
+ *
+ */
+extern void TM_ILI9341_Button_DrawAll(void);
+
+/**
+ * Enable button
+ *
+ * Parameters:
+ * 	- uint8_t id: button id
+ */
 extern void TM_ILI9341_Button_Enable(uint8_t id);
 
+/**
+ * Disable button
+ *
+ * Parameters:
+ * 	- uint8_t id: button id
+ */
 extern void TM_ILI9341_Button_Disable(uint8_t id);
 
+/**
+ * Delete all buttons in library
+ *
+ *
+ */
 extern void TM_ILI9341_Button_DeleteAll(void);
 
+/**
+ * Delete button
+ *
+ * Parameters:
+ * 	- uint8_t id: button id
+ */
 extern void TM_ILI9341_Button_Delete(uint8_t id);
 
+/**
+ * Get pressed button
+ *
+ * Parameters:
+ * 	- TM_STMPE811_TouchData *TouchData: pointer to TM_STMPE811_TouchData structure
+ *
+ * Returns button id on success, otherwise -1
+ */
 extern int8_t TM_ILI9341_Button_Touch(TM_STMPE811_TouchData *TouchData);
 
 #endif
