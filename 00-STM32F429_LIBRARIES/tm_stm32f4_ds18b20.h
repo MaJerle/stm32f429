fb8e1dc98439ad5cac2abca701710fe1de865315
tilz0R <tilen.majerle@gmail.com>
2014-05-17 11:48:29 +0200

    DS18B20 link to example added

--- 00-STM32F429_LIBRARIES/tm_stm32f4_ds18b20.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_ds18b20.h
@@ -7,7 +7,7 @@
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
- *	@link		
+ *	@link		http://bit.ly/1viCnSX
  *	@version 	v1.0
  *	@ide		Keil uVision
  */
