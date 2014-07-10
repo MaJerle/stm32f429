b1c8359c2ee47484f97c1d1ba449335eca84a21e
Tilen Majerle <tilen.majerle@gmail.com>
2014-07-10 18:30:34 +0200

    RNG Changes

--- 00-STM32F429_LIBRARIES/tm_stm32f4_rng.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_rng.h
@@ -6,7 +6,7 @@
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
- *	@link		
+ *	@link		http://bit.ly/1sCyx4w
  *	@version 	v1.0
  *	@ide		Keil uVision
  */
