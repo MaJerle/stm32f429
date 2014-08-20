afc34a5e31e70f877bee6cbc52bbb846c07a2786
Tilen Majerle <tilen.majerle@gmail.com>
2014-08-20 12:05:04 +0200

    defines.h edited

--- 27-STM32F429_GPS/User/defines.h
+++ 27-STM32F429_GPS/User/defines.h
@@ -1,15 +1,5 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define TM_SPI5_PRESCALER			SPI_BaudRatePrescaler_2
 
-#define TM_USART_BUFFER_SIZE		1024
-
-#define FATFS_USE_SDIO				0
-
-/*
-#define GPS_DISABLE_GPGSA
-#define GPS_DISABLE_GPGSV
-#define GPS_DISABLE_GPGGA
-*/
 #endif 
