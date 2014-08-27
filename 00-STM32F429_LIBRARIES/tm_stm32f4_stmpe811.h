324bd5c832f97ecb145c35ffe11def6c54dafdc0
Tilen Majerle <tilen.majerle@gmail.com>
2014-08-27 17:10:55 +0200

    Some updates

--- 00-STM32F429_LIBRARIES/tm_stm32f4_stmpe811.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_stmpe811.h
@@ -33,9 +33,18 @@
  *
  * Default pinout
  *
- * STMPE811		STM32F429 Discovery			Description
- * SCL			PA8							Clock pin for I2C
- * SDA			PC9							Data pin for I2C
+ * 	STMPE811		STM32F429 Discovery			Description
+ * 
+ * 	SCL				PA8							Clock pin for I2C
+ * 	SDA				PC9							Data pin for I2C
+ * 
+ * This pinout is used on STM32F429 Discovery board.
+ * If you want to change it, you can add lines below in your defines.h file
+ * 
+ *	//Use custom I2C settings. Check TM I2C library for corresponding pins
+ *	#define STMPE811_I2C					I2C3
+ *	#define STMPE811_I2C_PINSPACK			TM_I2C_PinsPack_1
+ * 
  */
 #ifndef TM_STMPE811_H
 #define TM_STMPE811_H	110
@@ -45,9 +54,8 @@
  * - STM32F4xx RCC
  * - STM32F4xx GPIO
  * - defines.h
- * - TM_I2C
- * - TM_DELAY
- * - TM_STMPE811
+ * - TM I2C
+ * - TM DELAY
  */
 /**
  * Includes
@@ -68,7 +76,10 @@
 /* I2C address */
 #define STMPE811_ADDRESS				0x82
 
-//Registers
+/* STMPE811 Chip ID on reset */
+#define STMPE811_CHIP_ID_VALUE			0x0811	//Chip ID
+
+/* Registers */
 #define STMPE811_CHIP_ID				0x00	//STMPE811 Device identification
 #define STMPE811_ID_VER					0x02	//STMPE811 Revision number; 0x01 for engineering sample; 0x03 for final silicon
 #define STMPE811_SYS_CTRL1				0x03	//Reset control
@@ -120,92 +131,6 @@
 #define STMPE811_TEMP_CTRL				0x60	//Temperature sensor setup
 #define STMPE811_TEMP_DATA				0x61	//Temperature data access port
 #define STMPE811_TEMP_TH				0x62	//Threshold for temperature controlled interrupt
-
-//STMPE811 Chip ID on reset
-#define STMPE811_CHIP_ID_VALUE			0x0811	//Chip ID
-
-//Register STMPE811_SYS_CTRL1 bits
-#define STMPE811_SYS_CTRL1_SOFTRESET 	0x02	//Reset the STMPE811 using the serial communication interface
-#define STMPE811_SYS_CTRL1_HIBERNATE	0x01	//Force the device into hibernation mode
-
-//Register STMPE811_SYS_CTRL2 bits
-#define STMPE811_CTRL2_TS_OFF			0x08	//Switch off the clock supply to the temperature sensor.
-#define STMPE811_CTRL2_GPIO_OFF			0x04	//Switch off the clock the supply to the GPIO.
-#define STMPE811_CTRL2_TSC_OFF  		0x02	//Switch off the clock supply to the touchscreen controller.
-#define STMPE811_CTRL2_ADC_OFF			0x01	//Switch off the clock supply to the ADC.
-
-//Register STMPE811_INT_EN bits
-#define STMPE811_INT_EN_GPIO    		0x80	//Any enabled GPIO interrupts
-#define STMPE811_INT_EN_ADC     		0x40	//Any enabled ADC interrupts
-#define STMPE811_INT_EN_TEMP_SENS		0x20	//Temperature threshold triggering
-#define STMPE811_INT_EN_FIFOEMPTY		0x10	//FIFO is empty
-#define STMPE811_INT_EN_FIFOFULL		0x08	//FIFO is full
-#define STMPE811_INT_EN_FIFOOFLOW		0x04	//FIFO is overflowed
-#define STMPE811_INT_EN_TH      		0x02	//FIFO is equal or above threshold value
-#define STMPE811_INT_EN_TOUCHDET		0x01	//Touch is detected
-
-//Register STMPE811_ADC_CTRL1 bits
-#define STMPE811_ADC_CTRL1_SAMPLE_M		0x07	//ADC conversion time (in number of clock) mask
-#define STMPE811_ADC_CTRL1_SAMPLE_S		4		//ADC conversion time (in number of clock) shift
-#define STMPE811_ADC_CTRL1_MOD			0x08	//Selects 10 or 12-bit ADC operation
-#define STMPE811_ADC_CTRL1_SEL			0x02	//Select between internal or external reference for the ADC
-
-//Register STMPE811_ADC_CTRL2 bits
-#define STMPE811_ADC_CTRL2_CLK1			0x00	//Selects the clock speed of ADC is 1.625 MHz typ
-#define STMPE811_ADC_CTRL2_CLK2 		0x01	//Selects the clock speed of ADC is 3.25 MHz typ
-#define STMPE811_ADC_CTRL2_CLK13		0x02	//Selects the clock speed of ADC is 6.5 MHz typ
-#define STMPE811_ADC_CTRL2_CLK4			0x03	//Selects the clock speed of ADC is 6.5 MHz typ 
-
-//Register STMPE811_TSC_CFG bits
-#define STMPE811_TSC_CFG_AVE_1  		0x00	//1 sample
-#define STMPE811_TSC_CFG_AVE_2			0x40	//2 sample
-#define STMPE811_TSC_CFG_AVE_4			0x80	//4 sample
-#define STMPE811_TSC_CFG_AVE_8  		0xC0	//8 sample
-#define STMPE811_TSC_CFG_DELAY_M		0x07	//Touch detect delay mask
-#define STMPE811_TSC_CFG_DELAY_S		3		//Touch detect delay shift
-#define STMPE811_TSC_CFG_SETTING_M		0x07	//Panel driver setting time mask
-#define STMPE811_TSC_CFG_SETTING_S		0		//Panel driver setting time shift
-
-//Register STMPE811_FIFO_STA FIFO bits
-#define STMPE811_FIFO_STA_OFLOW			0x80	//Reads 1 if FIFO is overflow
-#define STMPE811_FIFO_STA_FULL 			0x40	//Reads 1 if FIFO is full
-#define STMPE811_FIFO_STA_EMPTY			0x20	//Reads 1 if FIFO is empty
-#define STMPE811_FIFO_STA_TRIG  		0x10	//FIFO size is at or beyond the threshold value
-#define STMPE811_FIFO_STA_RESET 		0x01	//Resets FIFO, all data in FIFO are cleared
-
-//Register STMPE811_TSC_FRACTION_Z bits
-#define STMPE811_FRACTION_Z_M			0x7		//Pressure measurement accuracy value mask
-#define STMPE811_FRACTION_Z_S			0		//Pressure measurement accuracy value mask
-
-//Register STMPE811_TSC_I_DRIVE bits.
-#define STMPE811_TSC_I_DRIVE_20			0x00	//Set maximum current on the TSC driving channel is 20 mA, 35 mA max
-#define STMPE811_TSC_I_DRIVE_50 		0x01	//Set maximum current on the TSC driving channel is 50 mA, 80 mA max
-
-//Register STMPE811_TSC_CTRL bits
-#define STMPE811_TSC_CTRL_STA   		0x80	//TSC status
-#define STMPE811_TSC_CTRL_TRACK_M		0x70	//Tracking index mask
-#define STMPE811_TSC_CTRL_TRACK_S		4		//Tracking index mask
-#define STMPE811_TSC_CTRL_XYZ   		0x00	//TSC operating mode: X, Y, Z acquisition
-#define STMPE811_TSC_CTRL_XY    		0x02	//TSC operating mode: only X, Y
-#define STMPE811_TSC_CTRL_X     		0x04	//TSC operating mode: only X
-#define STMPE811_TSC_CTRL_Y     		0x06	//TSC operating mode: only Y
-#define STMPE811_TSC_CTRL_Z     		0x08	//TSC operating mode: only Z
-#define STMPE811_TSC_CTRL_EN    		0x01	//Enable TSC
-
-//Register STMPE811_INT_STA bits
-#define STMPE811_INT_STA_GPIO   		0x80	//Any enable GPIO interrupts
-#define STMPE811_INT_STA_ADC    		0x40	//Any enable ADC interrupts
-#define STMPE811_INT_STA_SENS   		0x20	//Temperature threshold triggering
-#define STMPE811_INT_STA_EMPTY  		0x10	//FIFO is empty
-#define STMPE811_INT_STA_FULL   		0x08	//FIFO is full
-#define STMPE811_INT_STA_OFLOW  		0x04	//FIFO is overflowed
-#define STMPE811_INT_STA_TH     		0x02	//FIFO is equal or above threshold value
-#define STMPE811_INT_STA_DET    		0x01	//Touch is detected
-
-//Register STMPE811_INT_CTRL bits
-#define STMPE811_INT_POLARITY   		0x04	//Sets the INT pin polarity
-#define STMPE811_INT_TYPE       		0x02	//Sets the type of interrupt signal required by the host
-#define STMPE811_INT_GLOBAL     		0x01	//Master enable for the interrupt system
 
 /**
  * Enum for set how to read x and y from controller
--- 10-STM32F429_STMPE811/User/main.c
+++ 10-STM32F429_STMPE811/User/main.c
@@ -14,39 +14,39 @@
 
 int main(void) {
 	char str[30];
-	//Create TouchData struct
+	/* Create TouchData struct */
 	TM_STMPE811_TouchData touchData;
 	
-	//Initialize system
+	/* Initialize system */
 	SystemInit();
 	
-	//Initialize LCD
+	/* Initialize LCD */
 	TM_ILI9341_Init();
-	//Fill with orange color
+	/* Fill with orange color */
 	TM_ILI9341_Fill(ILI9341_COLOR_ORANGE);
-	//Rotate LCD
+	/* Rotate LCD */
 	TM_ILI9341_Rotate(TM_ILI9341_Orientation_Portrait_2);
 	
-	//Initialize Touch
+	/* Initialize Touch */
 	if (TM_STMPE811_Init() != TM_STMPE811_State_Ok) {
 		TM_ILI9341_Puts(20, 20, "STMPE811 Error", &TM_Font_11x18, ILI9341_COLOR_ORANGE, ILI9341_COLOR_BLACK);
 		
 		while (1);
 	}
 	
-	//Select touch screen orientation
+	/* Select touch screen orientation */
 	touchData.orientation = TM_STMPE811_Orientation_Portrait_2;
 	
-	//Print some text
+	/* Print some text */
 	TM_ILI9341_Puts(20, 20, "Press on LCD", &TM_Font_11x18, ILI9341_COLOR_ORANGE, ILI9341_COLOR_BLACK);
 	TM_ILI9341_Puts(93, 310, "stm32f4-discovery.com", &TM_Font_7x10, ILI9341_COLOR_GREEN, ILI9341_COLOR_BLACK);
 	
 	while (1) {
 		if (TM_STMPE811_ReadTouch(&touchData) == TM_STMPE811_State_Pressed) {
-			//Touch valid
+			/* Touch valid */
 			sprintf(str, "Pressed    \n\nX: %03d\nY: %03d", touchData.x, touchData.y);
 			TM_ILI9341_Puts(20, 80, str, &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_ORANGE);
-			
+			/* Draw pixel on touch location */
 			TM_ILI9341_DrawPixel(touchData.x, touchData.y, 0x0000);
 		} else {
 			sprintf(str, "Not Pressed\n\n       \n      ");
