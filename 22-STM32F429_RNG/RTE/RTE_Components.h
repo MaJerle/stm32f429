de3e787035eb2946bcd8cdb523d1114c2b83bf47
Tilen Majerle <tilen.majerle@gmail.com>
2014-09-02 21:06:46 +0200

    RNG project

---
+++ 22-STM32F429_RNG/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project' 
+ * Target:  'STM32F429-Discovery' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_STM32F4xx    /* Device Startup for STM32F4 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_RNG
+#define RTE_DEVICE_STDPERIPH_USART
+
+#endif /* RTE_COMPONENTS_H */
--- 22-STM32F429_RNG/User/main.c
+++ 22-STM32F429_RNG/User/main.c
@@ -1,13 +1,17 @@
 /**
- *	Keil project for true random number generator
+ *	Keil project for Random Number Generator
+ *
+ *	Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
  *	@ide		Keil uVision 5
  */
-#include "defines.h"
+/* Include core modules */
 #include "stm32f4xx.h"
+/* Include my libraries here */
+#include "defines.h"
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_usart.h"
 #include "tm_stm32f4_rng.h"
@@ -16,17 +20,22 @@
 int main(void) {
 	char buffer[20];
 	
-	//Initialize system
+	/* Initialize system */
 	SystemInit();
-	//Initialize delay
+	
+	/* Initialize delay */
 	TM_DELAY_Init();
-	//Initialize USART1, TX: PA9, RX: PA10
-	TM_USART_Init(USART1, TM_USART_PinsPack_1, 115200);	
-	//Initialize random number generator
+	
+	/* Initialize USART1, TX: PB6 */
+	TM_USART_Init(USART1, TM_USART_PinsPack_2, 115200);	
+	
+	/* Initialize random number generator */
 	TM_RNG_Init();
 	
 	while (1) {
+		/* Get number */
 		sprintf(buffer, "Number: %u\n", TM_RNG_Get());
+		/* Put to USART */
 		TM_USART_Puts(USART1, buffer);
 		Delayms(1000);
 	}
