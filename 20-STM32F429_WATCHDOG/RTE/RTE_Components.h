d62c8ddddfac69ec9106d0906f58fd598b1c10cc
Tilen Majerle <tilen.majerle@gmail.com>
2014-09-02 21:19:40 +0200

    Watchdog updated

---
+++ 20-STM32F429_WATCHDOG/RTE/RTE_Components.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'project' 
+ * Target:  'STM32F429-Discovery' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_STM32F4xx    /* Device Startup for STM32F4 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_IWDG
+#define RTE_DEVICE_STDPERIPH_RCC
+
+#endif /* RTE_COMPONENTS_H */
--- 20-STM32F429_WATCHDOG/User/main.c
+++ 20-STM32F429_WATCHDOG/User/main.c
@@ -1,42 +1,49 @@
 /**
- *	Keil project
+ *	Keil project for independent watchdog
+ *
+ *	Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
  *	@ide		Keil uVision 5
  */
-#include "defines.h"
+/* Include core modules */
 #include "stm32f4xx.h"
+/* Include my libraries here */
+#include "defines.h"
 #include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_disco.h"
 #include "tm_stm32f4_watchdog.h"
 
 int main(void) {
-	//Initialize system
+	/* Initialize system */
 	SystemInit();
-	//Initialize delay
+	
+	/* Initialize delay */
 	TM_DELAY_Init();
-	//Initialize leds on board
+	
+	/* Initialize leds on board */
 	TM_DISCO_LedInit();
-	//Initialize button
+	
+	/* Initialize button */
 	TM_DISCO_ButtonInit();
-	//Initialize watchdog timer
-	//Set timeout to 1s
+	
+	/* Initialize watchdog timer */
+	/* Set timeout to 1s */
 	if (TM_WATCHDOG_Init(TM_WATCHDOG_Timeout_1s)) {
-		//System was reset by watchdog
+		/* System was reset by watchdog */
 		TM_DISCO_LedOn(LED_RED);
 	} else {
-		//System was not reset by watchdog
+		/* System was not reset by watchdog */
 		TM_DISCO_LedOn(LED_GREEN);
 	}
 	
 	while (1) {
-		//if button is pressed, do nothing and system will be reset after 1 second
+		/* If button is pressed, do nothing and system will be reset after 1 second */
 		while (TM_DISCO_ButtonPressed());
 		
-		//Reset watchdog
+		/* Reset watchdog */
 		TM_WATCHDOG_Reset();
 	}
 }
-
