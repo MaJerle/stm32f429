6f65079e8cd67e5150da7d722ddf24e0565468fc
Tilen Majerle <tilen.majerle@gmail.com>
2014-09-02 23:26:18 +0200

    LEDs and Buttons updated

--- 02-STM32F429_LED_BUTTON/User/main.c
+++ 02-STM32F429_LED_BUTTON/User/main.c
@@ -1,12 +1,17 @@
 /**
- *	Leds and button example
+ *	Keil project for LEDS and BUTTONS on Discovery and Nucleo boards
+ *
+ *	Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
- *	@website	http://majerle.eu
- *	@ide		Keil uVision
+ *	@website	http://stm32f4-discovery.com
+ *	@ide		Keil uVision 5
  */
+/* Include core modules */
 #include "stm32f4xx.h"
+/* Include my libraries here */
+#include "defines.h"
 #include "tm_stm32f4_disco.h"
 
 int main(void) {
