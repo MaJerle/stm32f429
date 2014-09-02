d653f64b334f1ed6b3c0e1f79bdf912542ca13e0
Tilen Majerle <tilen.majerle@gmail.com>
2014-09-02 23:21:52 +0200

    SDRAM and I2C updates

--- 09-STM32F429_I2C/User/main.c
+++ 09-STM32F429_I2C/User/main.c
@@ -1,12 +1,16 @@
 /**
- *	Keil project for I2C example
+ *	Keil project for I2C peripheral
+ *
+ *	Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
- *	@website	http://majerle.eu
+ *	@website	http://stm32f4-discovery.com
  *	@ide		Keil uVision 5
  */
+/* Include core modules */
 #include "stm32f4xx.h"
+/* Include my libraries here */
 #include "defines.h"
 #include "tm_stm32f4_i2c.h"
 #include "tm_stm32f4_delay.h"
@@ -16,13 +20,14 @@
 
 int main(void) {
 	uint8_t data[] = {0, 1, 2};
-	//Initialize system
+	
+	/* Initialize system */
 	SystemInit();
 
-	//Initialize I2C, SCL: PB6 and SDA: PB7 with 100kHt serial clock
+	/* Initialize I2C, SCL: PB6 and SDA: PB7 with 100kHt serial clock */
 	TM_I2C_Init(I2C1, TM_I2C_PinsPack_1, 100000);
 
-	//Write "5" at location 0x00 to slave with address ADDRESS
+	/* Write "5" at location 0x00 to slave with address ADDRESS */
 	TM_I2C_Write(I2C1, ADDRESS, 0x00, 5);
 	
 	/**
@@ -31,7 +36,7 @@ int main(void) {
 	 */
 	TM_I2C_WriteMulti(I2C1, ADDRESS, 0x00, data, 3);
 	
-	//Read single byte from slave with 0xD0 (1101 000 0) address and register location 0x00
+	/* Read single byte from slave with 0xD0 (1101 000 0) address and register location 0x00 */
 	data[0] = TM_I2C_Read(I2C1, ADDRESS, 0x00);
 	
 	/**
@@ -45,4 +50,3 @@ int main(void) {
 
 	}
 }
-
--- 14-STM32F429_SDRAM/User/main.c
+++ 14-STM32F429_SDRAM/User/main.c
@@ -1,13 +1,17 @@
 /**
- *	SDRAM example for STM32F429 Discovery board only
+ *	Keil project for SDRAM connected on STM32F429 Discovery board
+ *
+ *	Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
  *	@ide		Keil uVision 5
  */
-#include "defines.h"
+/* Include core modules */
 #include "stm32f4xx.h"
+/* Include my libraries here */
+#include "defines.h"
 #include "tm_stm32f4_disco.h"
 #include "tm_stm32f4_sdram.h"
 #include "tm_stm32f4_delay.h"
@@ -15,33 +19,33 @@
 int main(void) {
 	uint16_t write, read;
 	uint8_t i;
-	//Initialize system
+	/* Initialize system */
 	SystemInit();
 	
-	//Initialize delay
+	/* Initialize delay */
 	TM_DELAY_Init();
 	
-	//Initialize leds
+	/* Initialize leds */
 	TM_DISCO_LedInit();
 	
-	//Initialize SDRAM
+	/* Initialize SDRAM */
 	if (TM_SDRAM_Init()) {
 		TM_DISCO_LedOn(LED_GREEN);
 	} else {
 		TM_DISCO_LedOn(LED_RED);
 	}
-	//Some delay
+	/* Some delay */
 	Delayms(2000);
 	
 	write = 1234;
-	//Write 16bit value to SDRAM at location 0x3214
+	/* Write 16bit value to SDRAM at location 0x3214 */
 	TM_SDRAM_Write16(0x3214, write);
-	//Read from location 0x3214
+	/* Read from location 0x3214 */
 	read = TM_SDRAM_Read16(0x3214);
 	
 	if (write == read) {
 		TM_DISCO_LedOff(LED_GREEN | LED_RED);
-		//Blink leds to indicate that reading and writing was correct
+		/* Blink leds to indicate that reading and writing was correct */
 		for (i = 0; i < 10; i++) {
 			TM_DISCO_LedToggle(LED_GREEN | LED_RED);
 			Delayms(100);
