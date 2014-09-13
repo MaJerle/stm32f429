ff84480b2b50fa25f29e62c40c478508129f77d5
Tilen Majerle <tilen.majerle@gmail.com>
2014-09-14 00:38:05 +0200

    defines.h updated

--- 34-STM32F429_USB_HID_DEVICE/User/defines.h
+++ 34-STM32F429_USB_HID_DEVICE/User/defines.h
@@ -9,7 +9,7 @@
 //#define USE_USB_OTG_HS	/* STM32F429-Discovery */
 
 /* Uncomment if you want to disable ID pin for USB HID Device library */
-#define USB_HID_DEVICE_DISABLE_ID
+//#define USB_HID_DEVICE_DISABLE_ID
 
 /* Uncomment if you want to disable VBUS pin for USB HID HOST library */
 /* If you do this, on F4 and F429 Discovery boards USB will not work */
