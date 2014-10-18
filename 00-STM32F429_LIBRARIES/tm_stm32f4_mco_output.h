8faeba58fad0084b09a21b05e4076c55f16c12fa
Tilen Majerle <tilen.majerle@gmail.com>
2014-10-19 01:52:38 +0200

    MCO output updated

--- 00-STM32F429_LIBRARIES/tm_stm32f4_mco_output.h
+++ 00-STM32F429_LIBRARIES/tm_stm32f4_mco_output.h
@@ -4,7 +4,7 @@
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
- *	@link		
+ *	@link		http://stm32f4-discovery.com/2014/10/library-40-output-clocks-stm32f4/
  *	@version 	v1.0
  *	@ide		Keil uVision
  *	@license	GNU GPL v3
