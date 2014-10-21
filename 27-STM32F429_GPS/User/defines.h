013790f9a4b6a2d3585d2b69bfff6ed8cd34e6dc
Tilen Majerle <tilen.majerle@gmail.com>
2014-10-21 11:36:19 +0200

    GPS defines.h updated

--- 27-STM32F429_GPS/User/defines.h
+++ 27-STM32F429_GPS/User/defines.h
@@ -30,8 +30,23 @@
 
 /* Put your global defines for all libraries here used in your project */
 
-/* Set USART buffer length to 256 bytes */
+/* Set USART receive buffer length to 256 bytes */
 #define TM_USART_BUFFER_SIZE	256
+
+/* Change custom USART */
+/* USART1 is selected by default, if you want custom setting, */
+/* then uncomment lines below and edit them. */
+//#define GPS_USART				USART1
+//#define GPS_USART_PINSPACK	TM_USART_PinsPack_2
+
+/* By default, each of this data has to be detected in order to get "VALID" data. */
+/* If your GPS does not return any of this statement, you can disable option. */
+/* If you disable any of statements, then you will loose data, corresponding to statement. */
+/* Uncomment line which statement is not supported by your GPS */
+//#define GPS_DISABLE_GPGGA 	/* Disable GPGGA statement */
+//#define GPS_DISABLE_GPRMC		/* Disable GPRMC statement */
+//#define GPS_DISABLE_GPGSA		/* Disable GPGSA statement */
+//#define GPS_DISABLE_GPGSV		/* Disable GPGSV statement */
 
 #endif
 
