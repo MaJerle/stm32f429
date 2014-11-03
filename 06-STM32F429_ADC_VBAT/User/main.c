42563a2cd255d4b7642d855aa8dc97cf07810d35
Tilen Majerle <tilen.majerle@gmail.com>
2014-11-03 22:26:55 +0100

    New

--- 06-STM32F429_ADC_VBAT/User/main.c
+++ 06-STM32F429_ADC_VBAT/User/main.c
@@ -1,13 +1,16 @@
 /**
  *	Keil project for ADC peripheral how to measure Vbat pin
  *
- *	Before you start, select your target, on the right of the "Load" button
+ *  Before you start, select your target, on the right of the "Load" button
  *
  *	@author 	Tilen Majerle
  *	@email		tilen@majerle.eu
  *	@website	http://stm32f4-discovery.com
  *	@ide		Keil uVision 5
+ *	@packs		STM32F4xx Keil packs version 2.2.0 or greater required
+ *	@stdperiph	STM32F4xx Standard peripheral drivers version 1.4.0 or greater required
  */
+
 /* Include core modules */
 #include "stm32f4xx.h"
 /* Include my libraries here */
