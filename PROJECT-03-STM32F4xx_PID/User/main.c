64ea3d9573bc734e2c86e407b05219c9af25c7fb
Tilen Majerle <tilen.majerle@gmail.com>
2014-11-23 11:40:45 +0100

    PID project updated

--- PROJECT-03-STM32F4xx_PID/User/main.c
+++ PROJECT-03-STM32F4xx_PID/User/main.c
@@ -35,6 +35,7 @@
 #define TEMP_CURRENT		temps[1]	/* Temperature we actually have */
 #define TEMP_WANT			temps[0]	/* Temperature we want to have */
 
+/* Choose PID parameters */
 #define PID_PARAM_KP		100			/* Proporcional */
 #define PID_PARAM_KI		0.025		/* Integral */
 #define PID_PARAM_KD		20			/* Derivative */
@@ -165,4 +166,3 @@ int main(void) {
 		}
 	}
 }
-
