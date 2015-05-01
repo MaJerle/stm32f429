16d479f741e2ad75535161f96b9604c1c5c496ff
Tilen Majerle <tilen.majerle@gmail.com>
2015-05-01 15:35:06 +0200

    Updates

--- 21-STM32F429_FATFS_SDRAM/User/main.c
+++ 21-STM32F429_FATFS_SDRAM/User/main.c
@@ -1,5 +1,5 @@
 /**
- *	Keil project template
+ *	Keil project template for using FATFS with SDRAM
  *
  *	Before you start, select your target, on the right of the "Load" button
  *
@@ -10,6 +10,8 @@
  *	@conf		PLL parameters are set in "Options for Target" -> "C/C++" -> "Defines"
  *	@packs		STM32F4xx Keil packs version 2.4.0 or greater required
  *	@stdperiph	STM32F4xx Standard peripheral drivers version 1.5.0 or greater required
+ *
+ * Check defines.h file for configuration settings about SDRAM and FATFS
  */
 /* Include core modules */
 #include "stm32f4xx.h"
@@ -43,6 +45,8 @@ int main(void) {
 	fres = f_mount(&fs, "SDRAM:", 0);
 	
 	/* Create FAT volume with default cluster size, IMPORTANT for SDRAM because content is not stored forever! */
+	/* This has to be done only once, when you first time init your SDRAM */
+	/* It is initialized first time using f_mount() function call above */
 	fres = f_mkfs("SDRAM:", 0, 0);
 	
 	/* Open file */
@@ -61,6 +65,8 @@ int main(void) {
 	if (fres == FR_OK) {
 		/* Mount FATFS on SDRAM */
 		fres = f_mount(&fs, "SDRAM:", 0);
+		
+		/* No need to call MKFS here again because fat system is already on SDRAM */
 		
 		/* Open file */
 		fres = f_open(&fil, "SDRAM:test.txt", FA_READ | FA_WRITE);
@@ -87,6 +93,7 @@ int main(void) {
 		TM_DISCO_LedOn(LED_RED);
 	}
 	
+	/* Main loop */
 	while (1) {
 		
 	}
