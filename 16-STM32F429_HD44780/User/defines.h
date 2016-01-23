e957b3b6e3f5af63d3cb5b5146be29390249a47f
Tilen Majerle <tilen.majerle@gmail.com>
2016-01-23 11:18:30 +0100

    Defines.h file example updated

--- 16-STM32F429_HD44780/User/defines.h
+++ 16-STM32F429_HD44780/User/defines.h
@@ -31,35 +31,28 @@
 /* Put your global defines for all libraries here used in your project */
 
 /* If you want to change your pinout
-/ then uncomment pin you want to change
-/ and edit its settings */
+/* then uncomment pin you want to change
+/* and edit its settings */
 
-/* Control pins */
 /* RS - Register select pin */
-//#define TM_HD44780_RS_RCC				RCC_AHB1Periph_GPIOB
-//#define TM_HD44780_RS_PORT			GPIOB
-//#define TM_HD44780_RS_PIN				GPIO_Pin_2
+//#define HD44780_RS_PORT            GPIOB
+//#define HD44780_RS_PIN            GPIO_Pin_2
 /* E - Enable pin */
-//#define TM_HD44780_E_RCC				RCC_AHB1Periph_GPIOB
-//#define TM_HD44780_E_PORT				GPIOB
-//#define TM_HD44780_E_PIN				GPIO_Pin_7
-
+//#define HD44780_E_PORT            GPIOB
+//#define HD44780_E_PIN                GPIO_Pin_7
+ 
 /* D4 - Data 4 pin */
-//#define TM_HD44780_D4_RCC				RCC_AHB1Periph_GPIOC
-//#define TM_HD44780_D4_PORT			GPIOC
-//#define TM_HD44780_D4_PIN				GPIO_Pin_12
+//#define HD44780_D4_PORT            GPIOC
+//#define HD44780_D4_PIN            GPIO_Pin_12
 /* D5 - Data 5 pin */
-//#define TM_HD44780_D5_RCC				RCC_AHB1Periph_GPIOC
-//#define TM_HD44780_D5_PORT			GPIOC
-//#define TM_HD44780_D5_PIN				GPIO_Pin_13
+//#define HD44780_D5_PORT            GPIOC
+//#define HD44780_D5_PIN            GPIO_Pin_13
 /* D6 - Data 6 pin */
-//#define TM_HD44780_D6_RCC				RCC_AHB1Periph_GPIOB
-//#define TM_HD44780_D6_PORT			GPIOB
-//#define TM_HD44780_D6_PIN				GPIO_Pin_12
+//#define HD44780_D6_PORT            GPIOB
+//#define HD44780_D6_PIN            GPIO_Pin_12
 /* D7 - Data 7 pin */
-//#define TM_HD44780_D7_RCC				RCC_AHB1Periph_GPIOB
-//#define TM_HD44780_D7_PORT			GPIOB
-//#define TM_HD44780_D7_PIN				GPIO_Pin_13
-
+//#define HD44780_D7_PORT            GPIOB
+//#define HD44780_D7_PIN            GPIO_Pin_13
+ 
 #endif
 
